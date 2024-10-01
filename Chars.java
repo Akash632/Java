fb35a93c17b6f9ca42f61ddfcc3d7512acaa03a1
Akash632 <akashnagineni632@gmail.com>
2024-10-01 20:11:33 +0530

    character methods

---
+++ Chars.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Chars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char let1 = 'a';
+        char let2 = '4';
+        char let3 = 'A';
+        char let4 = ' ';
+
+        // System.out.println(Character.isLetter(let1));  //true
+        // System.out.println(Character.isLetter(let2));  //false
+
+        // System.out.println(Character.isDigit(let1));  //false
+        // System.out.println(Character.isDigit(let2));  //true
+
+        // System.out.println(Character.isUpperCase(let3));  //true
+        // System.out.println(Character.isLowerCase(let1));  //true
+        // System.out.println(Character.isWhitespace(let4));  //true
+
+        // toUpperCase, toLowerCase works as same.
+
+        //toString converts the char into string
+    }
+}
