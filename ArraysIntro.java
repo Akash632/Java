06dd12b1f0578c02d540360da127fae19e3127b4
Akash632 <akashnagineni632@gmail.com>
2024-10-04 12:56:06 +0530

    Arrays

---
+++ ArraysIntro.java
@@ -0,0 +1,47 @@
+
+
+
+
+public class ArraysIntro {
+    public static void main(String[] args) {
+
+        //Arrays are used to store the data of same data type.
+
+        //int[] arr;  //array declaration
+
+        //This declaration doen't creates and array, it simple tells the compiler that the variable will hold an array.
+
+        //we can intialize, declare and create an array in the same line.
+
+        //int[] nums = {1,2,3,4,5}; //these elements creates the array an object and the reference of this array is assigned to nums.
+
+        //we can access array elements using indexes.
+
+        //Array provides a method toString[] to display at a time.
+
+         int[] nums = {1,2,3,4,5};
+
+        // System.out.println(Arrays.toString(nums));  //method to print arrays.
+
+        //we can also modify arrays using the index notation.
+
+        //we can create an array using new key word as well.
+
+        //int[] arr = new int[3];  //creating an array using new key word
+
+        //In the above code, this will allocate memory for 3 int values. Once the memory is allocated, it will assign value 0,
+
+        //incase of byte, short, int, long. For float and double it will 0.0 and for string it will be null.
+
+        //int size = arr.length;  //gives the size of the array.
+
+        // for(int i =0; i<nums.length; i++){    looping on array using for loop
+        //     System.out.println(nums[i]); 
+        // }
+
+        // for (int i : nums) {                   looping on array using for each.
+        //     System.out.println(i);
+        // }
+
+    }   
+}
