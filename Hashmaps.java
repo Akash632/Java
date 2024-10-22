351516ef37d2a04f19884432319ca2ec7f64e780
Akash632 <akashnagineni632@gmail.com>
2024-10-22 16:24:04 +0530

    hashmap methods

--- Hashmaps.java
+++ Hashmaps.java
@@ -23,5 +23,55 @@ public class Hashmaps {
         data.put("John", 78);
 
         System.out.println(data);
+
+        //to access the elements we use get method.
+
+        System.out.println(data.get("Akash"));
+
+        //to modify the elements, we have 2 ways. one is replace() and other is put()
+
+        //replace() replaces the old value with the new value and returns the old value 
+        //if no key is present it returns null.
+;
+        System.out.println(data.replace("Akash", 25));
+        System.out.println(data);
+
+        //put() method is also used to replace the old value with the new value and returns the old value, 
+        //if value is not present, it will add the key value and returns null.
+
+        System.out.println(data.put("Ajay",20));
+        System.out.println(data);
+
+
+        //to remove elements we have remove and clear
+
+        //If the HashMap contains the specified key, 
+        //remove()  will remove the element and returns the value mapped to the specified key. Otherwise, null is returned.
+
+        System.out.println(data.remove("Akash"));
+        System.out.println(data);
+
+        //clear removes all the elements from hashmap and makes it empty.
+
+        // data.clear();
+        // System.out.println(data);
+
+        //keySet() is used to get all the keys from a hashmap
+
+        System.out.println(data.keySet());  //returns a set which is an interface
+
+        //values() is used to get all the values from a hashmap
+
+        System.out.println(data.values());  //returns a set which is an interface
+
+        //Entryset is used to get all the entries
+
+        System.out.println(data.entrySet());   //returns a set which is an interface
+
+        //we use forEach loop to iterate over a hashmap
+
+        for(String key : data.keySet()){
+            System.out.println(key + ":" + data.get(key));
+        }
     }  
 }
