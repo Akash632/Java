a7cf1ad4238408bb71771671f3d79b4f04e5f2e4
Akash632 <akashnagineni632@gmail.com>
2024-10-25 15:14:48 +0530

    methods1 commit

--- MethodsInJava.java
+++ MethodsInJava.java
@@ -1,5 +1,48 @@
+import java.util.*;
 public class MethodsInJava {
+
+    // static void greet(){
+    //     System.out.println("Hello");
+    // }
+
+    static int addition(){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        return a+b;
+
+    }
+
+    static void printName(String name){
+        System.out.println("Hello"+ " "+ name);
+    }
     public static void main(String[] args){
-        System.out.println("Mehtods");
+
+
+        //A method must be declared before it is used any where in the program.
+
+        //accessModifier static returnType methodName(){}
+
+        //static methods in java can be called even without creating objects of the class.
+
+        //accessModifiers are used to spcify the accessibility of the method. public, private, default or protected.
+
+        //calling a method
+
+        //greet();
+
+        int result = addition();
+        System.out.println(result);
+
+        //we can create methods with parameters as well.
+
+        //accessmodifiers static returnType methodName(dataType value, dataType value, ......){}
+
+
+        printName("AKASH");
     }
 }
