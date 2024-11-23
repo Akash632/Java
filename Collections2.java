4de7df96b04fcb0a65702d43f015668a9f9e3848
Akash632 <akashnagineni632@gmail.com>
2024-11-23 16:09:54 +0530

    linked lists

--- Collections2.java
+++ Collections2.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Collections2 {
     public static void main(String[] args){
+
         ArrayList<Integer> nums = new ArrayList<>();
 
         nums.add(1);
-        nums.add(20);
+        nums.add(60);
         nums.add(50);
 
         // for(int i=0; i<nums.size();i++){
@@ -16,11 +18,27 @@ public class Collections2 {
         //     System.out.println(i);
         // }
 
-        nums.add(1, 30);
-        System.out.println(nums);
+        // nums.add(1, 30);
+        // System.out.println(nums);
 
-        nums.set(1,50);
-        System.err.println(nums);
+        // nums.set(1,50);
+        // System.err.println(nums);
+
+       // System.out.println(nums.getClass().getSimpleName());
+
+        //String[] arr = {"akash", "ashok"};
+
+       // ArrayList<String> nums1 = Arrays.asList(arr);
+
+       //ArrayList<String> names = new ArrayList<>(Arrays.asList(arr));
+
+       //nums.toArray();
+
+       //System.err.println(nums);
+
+       Collections.sort(nums);
+
+       System.out.println(nums);
 
 
     }
---
+++ Linked_lists.java
@@ -0,0 +1,10 @@
+
+
+public class Linked_lists {
+
+    public static void main(String[] args){
+        System.out.println("lINKED LISTS");
+        
+    }
+    
+}
