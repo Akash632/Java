129df3d1772581c53b766250d82ec170ac458e8c
Akash632 <akashnagineni632@gmail.com>
2024-11-23 17:06:33 +0530

    linked list collection

--- Linked_lists.java
+++ Linked_lists.java
@@ -1,10 +1,23 @@
 
+import java.util.LinkedList;
+
 
 public class Linked_lists {
-
     public static void main(String[] args){
-        System.out.println("lINKED LISTS");
-        
+        LinkedList<Integer> ll1 = new LinkedList<>();
+
+        ll1.add(1);
+        ll1.add(2);
+        ll1.add(3);
+        System.out.println(ll1);
+
+        System.out.println(ll1.get(1));
+
+        ll1.addFirst(20);
+        ll1.addLast(15);
+        System.out.println(ll1.getFirst());
+        ll1.getLast();
+        System.out.println(ll1);
     }
     
 }
