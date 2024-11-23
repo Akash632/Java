00128d920923e2b0aa6af974cb61cdce9c069c4d
Akash632 <akashnagineni632@gmail.com>
2024-11-23 13:33:38 +0530

    Array Lists

--- Collections2.java
+++ Collections2.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class Collections2 {
     public static void main(String[] args){
-        System.out.println("Hello collections");
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.add(1);
+        nums.add(20);
+        nums.add(50);
+
+        // for(int i=0; i<nums.size();i++){
+        //     System.out.println(nums.get(i));
+        // }
+        
+        // for(int i : nums){
+        //     System.out.println(i);
+        // }
+
+        nums.add(1, 30);
+        System.out.println(nums);
+
+        nums.set(1,50);
+        System.err.println(nums);
+
+
     }
 }
